0b8bbaf4d10e6dfbd0aef1885c6aba0e9257c773
FeetnotFound <119998971+FeetnotFound@users.noreply.github.com>
2025-09-27 16:44:28 -0700

    Delete Mirror directory
    just making sure

--- Mirror/Mirror/MirrorConnectivity.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-public func sendNowPlaying(_ info: NowPlayingInfo) async {
-    // Forward to the central MirrorManager which owns the connectivity manager
-    await MirrorManager.shared.sendNowPlaying(info)
-}
